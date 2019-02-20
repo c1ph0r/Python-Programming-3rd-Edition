07df6631b6c51d994b74463eb1c1c9126b1b1cb5
c1ph0r <c1ph0r@users.noreply.github.com>
2019-02-19 18:35:26 -0800

    Current Progress

---
+++ ch2_game_over_v2.py
@@ -0,0 +1,25 @@
+# Game Over - Version 2
+# Demonstrates the use of quotes in strings
+
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+print("Just",
+      "a bit",
+      "bigger")
+
+print("Here", end=" ")
+print("it is...")
+
+print(
+        """
+        GGGG  AAAA  MMMM  EEEE  OOOO  VVVV  EEEE  RRRR
+        GGGG  AAAA  MMMM  EEEE  OOOO  VVVV  EEEE  RRRR
+        GGGG  AAAA  MMMM  EEEE  OOOO  VVVV  EEEE  RRRR
+        GGGG  AAAA  MMMM  EEEE  OOOO  VVVV  EEEE  RRRR
+        GGGG  AAAA  MMMM  EEEE  OOOO  VVVV  EEEE  RRRR
+        """
+    )
+
+input("\n\nPress the enter key to exit.")
