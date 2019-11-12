4cf1d75ba5fe8915ca0a8f3e268c8177f0b6e24e
c1ph0r <c1ph0r@users.noreply.github.com>
2019-11-12 13:57:06 -0800

    Update ch5_heros_inventory_3.0.py

--- Chapter_5/ch5_heros_inventory_3.0.py
+++ Chapter_5/ch5_heros_inventory_3.0.py
@@ -6,40 +6,64 @@ inventory = ["sword",
              "armor",
              "shield",
              "healing potion"]
-print("\nYour items:")
+print("\nYour items:\n")
 for item in inventory:
     print(item)
-
-input("\n\nPress the enter key to continue.")
+input("\nPress the enter key to continue.")
 
 # get the length of a list
-print("You have", len(inventory), "items in your possession.")
-
-input("\n\nPress the enter key to continue.")
+print("\nYou have", len(inventory), "items in your possession.")
+input("\nPress the enter key to continue.")
 
 # test for membership with in
 if "healing potion" in inventory:
-    print("You will live to fight another day.")
+    print("\nYou will live to fight another day.")
 
 # display one item through an index
 index = int(input("\nEnter the index number for an item in inventory: "))
-print("At index", index, "is", inventory[index])
+print("\nAt index", index, "is", inventory[index])
 
 # display a slice
 start = int(input("\nEnter the index number to begin a slice: "))
 finish = int(input("\nEnter the index number to end the slice: "))
-print("inventory[", start, ":", finish, "] is", end=" ")
+print("\ninventory[", start, ":", finish, "] is", end=" ")
 print(inventory[start:finish])
-
-input("\n\nPress the enter key to continue.")
+input("\nPress the enter key to continue.")
 
 # concatenate two lists
 chest = ["gold", "gems"]
-print("You find a chest. It contains:")
+print("\nYou find a chest. It contains:\n")
 print(chest)
-print("You add the contents of the chest to your inventory.")
+print("\nYou add the contents of the chest to your inventory.")
 inventory += chest
-print("You inventory is now:")
+print("\nYou inventory is now:\n")
 print(inventory)
+input("\nPress the enter key to continue.")
 
-input("\n\nPress the enter key to exit.")
+# assign by index
+print("\nYou trade your sword for a crossbow.")
+inventory[0]= "crossbow"
+print("\nYour inventory is now:\n")
+print(inventory)
+input("\nPress the enter key to continue.")
+
+# assign by slice
+print("\nYou use your gold and gems to buy an orb of future telling.")
+inventory[4:6] = ["orb of future telling"]
+print("\nYour inventory is now:\n")
+print(inventory)
+input("\nPress the enter key to continue.")
+
+# delete an element
+print("\nIn a great battle , your shield is destroyed.")
+del inventory[2]
+print("\nYour inventory is now:\n")
+print(inventory)
+input("\nPress the enter key to continue.")
+
+# delete a slice
+print("\nYour crossbow and armor are stolen by thieves.")
+del inventory[:2]
+print("\nYour inventory is now:\n")
+print(inventory)
+input("\nPress the enter key to continue.")
