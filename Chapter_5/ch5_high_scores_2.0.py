5e546f99c9b504635afe67f31855e3cd9d8107bd
c1ph0r <c1ph0r@users.noreply.github.com>
2019-11-12 15:05:41 -0800

    Update ch5_high_scores_2.0.py

--- Chapter_5/ch5_high_scores_2.0.py
+++ Chapter_5/ch5_high_scores_2.0.py
@@ -1,6 +1,6 @@
 # High Scores 2.0
 # Demonstrates nested sequences
-
+import os
 scores = []
 
 choice = None
@@ -22,27 +22,30 @@ while choice != "0":
 
     # exit
     if choice == "0":
-        print("Goodbye.")
+        print("Goodbye.\n")
+        input("Press enter to close")
 
     # display high score table
     elif choice == "1":
-        print("High Scores:\n")
-        print("NAME\tSCORE")
+        os.system('CLS')
+        print("\nHigh Scores:\n")
+        print("NAME\t SCORE\n")
         for entry in scores:
             score, name = entry
-            print(name, "\t", score)
+            print(name,"\t",score)
 
     # add a score
     elif choice == "2":
-        name = input("What is the player's name?: ")
-        score = int(input("What score did the player get?: "))
+        os.system('CLS')
+        name = input("\nWhat is the player's name?: \n\n")
+        score = int(input("\nWhat score did the player get?: \n\n"))
         entry = (score, name)
         scores.append(entry)
         scores.sort(reverse=True)
         scores = scores[:5]     #keep only top 5 scores
+        os.system('CLS')
 
     # some unknown choice
     else:
+        os.system('CLS')
         print("Sorry, but", choice, "isn't a valid choice.")
-
-input("\nPress the enter key to exit.")
