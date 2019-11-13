3023c4143a1a495e74723e4efa8a5cb79c8e7696
c1ph0r <c1ph0r@users.noreply.github.com>
2019-11-13 14:06:03 -0800

    Update ch5_geek_translator.py

--- Chapter_5/ch5_geek_translator.py
+++ Chapter_5/ch5_geek_translator.py
@@ -1,6 +1,8 @@
 # Geek Translator
 # Demonstrates using dictionaries
 
+import os
+
 geek = {"404": "clueless.  From the web error message 404, meaning page not found.",
         "Googling": "searching the Internet for background information on a person.",
         "Keyboard Plaque" : "the collection of debris found in computer keyboards.",
@@ -11,6 +13,7 @@ geek = {"404": "clueless.  From the web error message 404, meaning page not foun
 choice = None
 
 while choice != "0":
+    os.system('CLS')
     print(
     """
     Geek Translator
@@ -28,52 +31,78 @@ while choice != "0":
 
     # exit
     if choice == "0":
-        print("Goodbye!")
+        os.system('CLS')
+        print("\nGoodbye!")
 
     # get a definition
     elif choice == "1":
-        term = input("What term do you want me to translate?: ")
+        os.system('CLS')
+        print("\n", geek.keys())
+        term = input("\nWhat term do you want me to translate?: ")
         if term in geek:
             definition = geek[term]
+            os.system('CLS')
             print("\n", term, "means", definition)
+            input("\n\nPress the enter key to continue.")
         else:
+            os.system('CLS')
             print("\nSorry, I don't know", term)
+            input("\n\nPress the enter key to continue.")
 
     # add a term-definition pair
     elif choice == "2":
-        term = input("What term do you want to add?: ")
+        os.system('CLS')
+        term = input("\nWhat term do you want to add?: ")
         if term not in geek:
             definition = input("\nWhat's the definition?: ")
             geek[term] = definition
+            os.system('CLS')
             print("\n", term, "has been added.")
+            input("\n\nPress the enter key to continue.")
         else:
+            os.system('CLS')
             print("\nThat term already exists! Try redefining it.")
+            input("\n\nPress the enter key to continue.")
 
     # redefine an existing term
     elif choice == "3":
-        term = input("What term do you want me to redefine?: ")
+        os.system('CLS')
+        term = input("\nWhat term do you want me to redefine?: ")
         if term in geek:
             definition = input("What's the new definition?: ")
             geek[term] = definition
+            os.system('CLS')
             print("\n", term, "has been redefine.")
+            input("\n\nPress the enter key to continue.")
         else:
+            os.system('CLS')
             print("\nThat term doesn't exist. Try adding it.")
+            input("\n\nPress the enter key to continue.")
 
     # delete a term-definition pair
     elif choice == "4":
-        term = input("What term do you want to delete?: ")
+        os.system('CLS')
+        term = input("\nWhat term do you want to delete?: ")
         if term in geek:
             del geek[term]
+            os.system('CLS')
             print("\nOkay I deleted", term)
+            input("\n\nPress the enter key to continue.")
         else:
+            os.system('CLS')
             print("\nI can't do that!", term, "doesn't exist in the dictionary.")
+            input("\n\nPress the enter key to continue.")
 
     # easter egg
     elif choice == "69":
-        print("Nice.")
+        os.system('CLS')
+        print("\nNice.")
+        input("\n\nPress the enter key to continue.")
 
     # some unknown choice
     else:
+        os.system('CLS')
         print("\nSorry, but", choice, "isn't a valid choice.")
+        input("\n\nPress the enter key to continue.")
 
 input("\n\nPress the enter key to exit.")
